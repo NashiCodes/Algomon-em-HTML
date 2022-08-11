5c1b534af58510d76ae99c7a2895ec9dd59d234a
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2022-08-11 11:44:16 -0300

    Simula structs do jogador e do algomon

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
 import mapa from "./mapa";
+
+function structAlgomon (nome, vida, atk, tipo){
+    this.nome = nome;
+    this.vida = vida;
+    this.atk = atk;
+    this.tipo = tipo;
+}
+
+function structPlayer (algm, Pname, line, column, algdex, insg){
+    this.algm = algm;
+    this.Pname = Pname;
+    this.line = line;
+    this.column = column;
+    this.algdex = algdex;
+    this.insg = insg;
+}
+
