aed7c44617fad64a65e6f5945df46134fd8e43fd
João Pereira <nashi.codes@gmail.com>
2022-08-13 13:18:43 -0300

    alteração no mapa.js e melhoria no main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
-import mapa from "./mapa.js";
-
-let box = document.querySelector('#box');
-let algcont = document.querySelector('#algmns');
-
+import layout from "./mapa.js";
+let box = document.querySelector("#box");
+let algcont = document.querySelector("#algmns");
 const L = 11;
 const C = 41;
 
-mapa.className = "map";
-mapa.id = "mId";
+layout.className = "map";
+layout.id = "mId";
 
-function criaAlgomon (nome, vida, atk, tipo){
+document.addEventListener("DOMContentLoaded", () => {
+  function criaAlgomon(nome, vida, atk, tipo) {
     this.nome = nome;
     this.vida = vida;
     this.atk = atk;
     this.tipo = tipo;
-}
-function criaPlayer (algm, Pname, algdex, insg){
-    this.algm =  new criaAlgomon();
+  }
+
+  function criaPlayer(algm, Pname, algdex, insg) {
+    this.algm = new criaAlgomon();
     this.Pname = Pname;
     this.algdex = algdex;
     this.insg = insg;
-}
-
+    }
+});
