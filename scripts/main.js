6a4cdcfeca79f72823ff9c8427d177f0e6564f0f
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2022-08-13 11:10:19 -0300

    inicia preparativos para printar mapa

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,23 @@
 import mapa from "./mapa.js";
 
+let box = document.querySelector('#box');
+
+const L = 11;
+const C = 41;
+
+mapa.className = "map";
+mapa.id = "mId";
+
 function criaAlgomon (nome, vida, atk, tipo){
     this.nome = nome;
     this.vida = vida;
     this.atk = atk;
     this.tipo = tipo;
 }
-
 function criaPlayer (algm, Pname, algdex, insg){
     this.algm =  new criaAlgomon();
-    this.nome = nome;
+    this.Pname = Pname;
     this.algdex = algdex;
     this.insg = insg;
 }
+
