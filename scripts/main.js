6830f6715a13a4bd35a45a9baaa7b664f3fabb90
João Pereira <nashi.codes@gmail.com>
2022-08-16 02:45:27 -0300

    troca de classes e erro na criação do player

--- scripts/main.js
+++ scripts/main.js
@@ -8,21 +8,23 @@ const height = 19;
 let gId = 0;
 let capId = 0;
 
-function criaAlgomon(nome, vida, atk, tipo) {
-  this.nome = nome;
-  this.vida = vida;
-  this.atk = atk;
-  this.tipo = tipo;
-  return this;
+class criaAlgomon {
+  constructor(nome, vida, atk, tipo) {
+    this.nome = nome;
+    this.vida = vida;
+    this.atk = atk;
+    this.tipo = tipo;
+  }
 }
 
-function criaPlayer(algm, Pname, algdex, insg, index) {
-  this.algm = [new criaAlgomon()];
-  this.Pname = Pname;
-  this.algdex = algdex;
-  this.insg = insg;
-  this.Index = index;
-  return this;
+class criaPlayer {
+  constructor( Pname, algdex, insg, index) {
+    this.algm = [new criaAlgomon()];
+    this.Pname = Pname;
+    this.algdex = algdex;
+    this.insg = insg;
+    this.Index = index;
+  }
 }
 
 layout.className = "map";
@@ -33,7 +35,44 @@ let player = new criaPlayer();
 player.Index = 422;
 player.algdex = 3;
 
-player.algm[0].nome = "Ifsauro";
+player.algm[0].nome = "Ifssauro";
+player.algm[0].atk = 5;
+player.algm[0].vida = 20;
+player.algm[0].tipo = "C";
+console.log(
+  player.algm[0].nome,
+  player.algm[0].atk,
+  player.algm[0].vida,
+  player.algm[0].tipo
+);
+
+
+
+player.algm[1].push.nome = "Whiledle";
+player.algm[1].atk = 3;
+player.algm[1].vida = 40;
+player.algm[1].tipo = "R";
+console.log(
+  player.algm[1].nome,
+  player.algm[1].atk,
+  player.algm[1].vida,
+  player.algm[1].tipo
+);
+
+// player.algm[2].nome = "vectray";
+// player.algm[2].atk = 4;
+// player.algm[2].vida = 30;
+// player.algm[2].tipo = "D";
+// console.log(
+//   player.algm[2].nome,
+//   player.algm[2].atk,
+//   player.algm[2].vida,
+//   player.algm[2].tipo
+// );
+
+///////////////////////////////////////////////
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const divs = [];
@@ -127,7 +166,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  function recebeAlgomon(id) {
+  function recebeAlgomon(_id) {
     player.algdex++;
     algcont.textContent = player.algdex;
   }
